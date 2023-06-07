645b6303aebc6a78b5f98822ac805f20ca70071a
MaybeAN <mailnitti@gmail.com>
2023-06-07 16:30:54 +0200

    Frontend

---
+++ src/main/webapp/HttpServletResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.io.PrintWriter;
+
+public class HttpServletResponse {
+    public void setContentType(String contentType) {
+    }
+
+    public PrintWriter getWriter() {
+        return null;
+    }
+}
---
+++ src/main/webapp/loadNavbars.js
@@ -0,0 +1,15 @@
+function includeHTML(elementId, fileUrl) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(elementId).innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", fileUrl, true);
+    xhttp.send();
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    includeHTML("navbar", "nav.html");
+    includeHTML("navbar2", "barrainferiore.html");
+});
